cb9e2bb60395e9b6f6281a009007d66711d233f6
Sp00nyMan <mrstormproxdd@gmail.com>
2022-01-22 18:24:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,148 +1,91 @@
-import kivy
-from kivy.config import Config
-from kivy.graphics import Color, Rectangle
-from kivy.graphics.texture import Texture
-
-Config.set('modules', 'monitor', '')
-
 from kivy.app import App
-from kivy.uix.image import Image
-from kivy.core.camera import Camera as CoreCamera
-from kivy.properties import NumericProperty, ListProperty, BooleanProperty, ObjectProperty
-from kivy.lang import Builder
+from kivy.core.image import Texture
+from kivy.graphics import Color, Rectangle
 from kivy.uix.floatlayout import FloatLayout
+
+from kivy.config import Config
+Config.set('modules', 'monitor', '')
+Config.set('modules', 'showborder', '')
+
 from kivy.utils import platform
-import numpy as np
 
 if platform == 'android':
     from android.permissions import request_permissions, Permission, check_permission
 
-Builder.load_string('''
-#: import Window kivy.core.window.Window
-<ScanCamera>:
+from kivy.core.camera import Camera as CoreCamera, CameraBase
+from kivy.uix.image import Image
+from kivy.properties import NumericProperty, ListProperty, BooleanProperty, ObjectProperty
+from kivy.core.window import Window
+import numpy as np
 
-<BarcWin>:
-    ScanCamera:
-        pos_hint: {'top': 0.9, 'right': 1}
-        size_hint: [1, 0.8]
-        canvas.before:
-            PushMatrix
-            Rotate:
-                angle: -90
-                origin: self.center
-        canvas.after:
-            PopMatrix
-            Line:
-                width: 2.
-                rectangle: (self.x + 40, self.y + 40, self.width/1.1, self.height/1.12)
-    ToggleButton:
-        id: show_bcode
-        pos_hint: {'bottom': 1, 'right': 1}
-        size_hint: [1, 0.1]
-        color: (1,1,1,1)
-        background_color: (0,0,0,0)
-        background_normal: ''
-        canvas.before:
-            Color:
-                rgba: (.18,.36,.61,1) if self.state=='down' else (0,0,0,1) 
-            Rectangle:
-                pos: self.pos
-                size: self.size
-        text: 'Hier kom die barcode...'
+from kivy.lang import Builder
+
+Builder.load_string('''
+<CameraControl>:
+    resolution: [2190, 1080]
+    allow_stretch: True
+    
+    canvas.before:
+        PushMatrix
+        Rotate:
+            angle: -90 if app.isAndroid else 0
+            origin: self.center
+    canvas.after:
+        PopMatrix
 ''')
 
 
-class BarcWin(FloatLayout):
-    cam_cam = ObjectProperty(None)
-
-    def __init__(self, **kwargs):
-        super(BarcWin, self).__init__(**kwargs)
-        self.cam_cam = ScanCamera()
-
-    def accept_in(self):
-        print('In')
-
-    def accept_out(self):
-        print('Out')
-
-
-class ScanCamera(Image):
-    play = BooleanProperty(True)
+class CameraControl(Image):
     index = NumericProperty(-1)
-    resolution = ListProperty([1920, 1080])
-    app_ini_ = ObjectProperty(None)
+    play = BooleanProperty(True)
+    resolution = ListProperty([-1, -1])
 
     def __init__(self, **kwargs):
+        super(CameraControl, self).__init__(**kwargs)
+        self.index = max(self.index, 0)
         self._camera = None
-        super(ScanCamera, self).__init__(**kwargs)
-        if self.index == -1:
-            self.index = 0
-        on_index = self._on_index
-        fbind = self.fbind
-        fbind('index', on_index)
-        fbind('resolution', on_index)
-        on_index()
-        self.app_ini_ = App.get_running_app()
 
-    def on_tex(self, *l):
-        self.canvas.ask_update()
-        if self.texture:
-            print(len(self.texture.pixels))
-            mask : Texture = Texture.create(size=self.texture.size, colorfmt='rgba')
-            texture = np.array(([255, 0, 0, 255] * (len(self.texture.pixels) // 8) + [0, 0, 0, 255] * (len(self.texture.pixels) // 8)), dtype='uint8').tostring()
-            mask.blit_buffer(texture, colorfmt='rgba')
-            Color(1, 1, 1, 1)
-            Rectangle(texture=mask, size=self.texture.size)
+        on_change = self._on_change
+        self.bind(index=on_change)
+        self.bind(resolution=on_change)
 
-    def _on_index(self, *largs):
-        self._camera = None
-        if self.index < 0:
+        self._on_change()
+
+    def _on_change(self, *l):
+        if self.index < 0: #or self.resolution[0] < 0:
             return
-        if self.resolution[0] < 0 or self.resolution[1] < 0:
-            return
-        # first init of corecamera object
-        if platform == 'android' and not check_permission(Permission.CAMERA):
-            raise RuntimeError('Camera permission denied!')
-        self._camera = CoreCamera(index=self.index,
-                                  resolution=self.resolution, stopped=True, size=self.size)
+        self._camera : CameraBase = CoreCamera(index=self.index, size=self.size, resolution=self.resolution)
 
-        # when camera loads call _camera_loaded method to bind corecamera method with uix.image texture
-        self._camera.bind(on_load=self._camera_loaded)
         if self.play:
             self._camera.start()
-            self._camera.bind(on_texture=self.on_tex)
+            self._camera.bind(on_texture=self.update)
 
-    def _camera_loaded(self, *largs):
-        # bind camera texture with uix.image texture that is still equal to None
-        self.texture = self._camera.texture
+    def update(self, *l):
+        self.canvas.ask_update()
+        print(f"Window = {Window.size}")
+        print(f"Widget = {self.size}")
+        print(f"Image = {self.norm_image_size}")
 
-    def on_play(self, instance, value):
-        if not self._camera:
-            return
-        if value:
-            self._camera.start()
-        else:
-            self._camera.stop()
+        if not self.texture:
+            self.texture = Texture.create(size=self._camera.texture.size, colorfmt=self._camera.texture.colorfmt)
+            self.texture.flip_vertical()
+        self.texture.blit_buffer(self._camera.texture.pixels, colorfmt=self._camera.texture.colorfmt)
 
+    def stop(self):
+        self._camera.stop()
 
 class TestCamera(App):
     title = 'Scan Camera'
+    isAndroid = False
 
     def build(self):
         if platform == 'android':
             request_permissions([Permission.CAMERA])
-        return BarcWin()
+            self.isAndroid = True
+        return CameraControl()
 
     def on_stop(self):
-        cc = ScanCamera()
-        cc._camera.stop()
-        print('Stop')
-
-    def on_pause(self):
-        return True
-
-    def on_resume(self):
-        pass
+        self.root.stop()
 
 if __name__ == '__main__':
     TestCamera().run()
